49e644cda891ca06222d0054a406e04d3d6db265
technicallytexan <dustin@technicallytexan.com>
2016-06-07 16:21:24 -0500

    Added commenses

--- xyrsFIX.py
+++ xyrsFIX.py
@@ -2,6 +2,8 @@ __author__ = 'Dustin'
 
 import sys, os, re
 
+# Find file named XXXX.xyrs or XXXX.txt where XXXX is a three or four digit integer (panel number)
+# This is generally what I name the XYRS files as I work with them
 for file in os.listdir():
     if re.search("^\d{3,}.(xyrs|txt)", file):
         file_in = file
@@ -19,6 +21,9 @@ xyrsOUT = open('xyrs.out', 'w')
 
 xyrsIN_list = list(xyrsIN)
 
+# Each capacitor, resistor, diode, and other 3-pin package components need to be offset
+# Q designators tend to be the three-pole SOT-23 type components and usually have a different offset.
+# TODO: These offset should be tested across a few different panels.
 for element in xyrsIN_list:
     if element[0] == 'C':
         strsplit = element.split("\t")
